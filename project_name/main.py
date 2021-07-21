e0b6e7122d6d2415fa421babae6972ef02bcca33
Alexander Sosedkin <monk@unboiled.info>
2021-07-21 02:05:20 +0200

    initial commit

---
+++ project_name/main.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+
+def main():
+    print('Hello world!')  # TODO: expand a little bit
+
+
+if __name__ == '__main__':
+    main()
---
+++ shell.nix
@@ -0,0 +1,15 @@
+{ pkgs ? import <nixpkgs> { } }:
+pkgs.mkShell {
+  buildInputs = with pkgs; [
+    # TODO: add generic dependencies
+    (python3.withPackages (ps: with ps; [
+      # TODO: add python dependencies
+    ]))
+  ];
+  nativeBuildInputs = with pkgs.python3Packages; [
+    pytest
+    flake8
+    flake8-import-order
+    codespell
+  ];
+}
