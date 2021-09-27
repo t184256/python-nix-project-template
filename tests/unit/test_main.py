ab197b6b4e29c3a07d9bbc685798f8666bfbf886
Daria Sosedkina <denichsv@gmail.com>
2021-09-27 22:38:51 +0200

    test

--- tests/unit/test_main.py
+++ tests/unit/test_main.py
@@ -1,5 +1,12 @@
 # TODO: expand a little
 
 def test_main():
+    from unittest import mock
+    from unittest.mock import patch
+    import io
     import project_name.main
     assert 'main' in dir(project_name.main)
+    with mock.patch('sys.stdout', new=io.StringIO()) as stdout_main:
+	    main()
+    assert stdout_main.getvalue() == 'Hello world!\n'
+
